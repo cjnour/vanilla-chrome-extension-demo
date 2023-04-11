cddd7a704aabf87c9145499e00d2e3bff01f8319
cjnour <charbel.nour9@gmail.com>
2023-04-11 18:47:02 -0400

    Fixed styles.

--- background.js
+++ background.js
@@ -8,7 +8,6 @@ chrome.contextMenus.create(menuItem);
 
 chrome.contextMenus.onClicked.addListener(function (clickData) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
-    // console.log("Click data: ", clickData, "Tabs: ", tab);
     chrome.scripting.executeScript({
       target: { tabId: tab[0].id },
       files: ["contentScript.js"],

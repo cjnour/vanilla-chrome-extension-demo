d7d403669e7531912cdd4452acbaa624cc812dcd
cjnour <charbel.nour9@gmail.com>
2023-04-11 22:53:20 -0400

    Removed integrated build and added iframe to build

--- contentScript.js
+++ contentScript.js
@@ -1,22 +1,8 @@
-// $.get(chrome.runtime.getURL("/modal.html"), function (data) {
-//   $($.parseHTML(data)).appendTo("body");
-// });
-
-// const init = function () {
-//   const injectElement = document.createElement("div");
-//   injectElement.className = "rustyZone-element";
-//   injectElement.innerHTML = "hello";
-//   document.body.appendChild(injectElement);
-// };
-// init();
-
-// document.body.style.backgroundColor = "orange";
 try {
   fetch(chrome.runtime.getURL("clientIndex.html"))
     .then((r) => r.text())
     .then((html) => {
       document.body.insertAdjacentHTML("afterbegin", html);
-      // not using innerHTML as it would break js event listeners of the page
     });
 } catch (err) {
   console.log("Error: ", err);

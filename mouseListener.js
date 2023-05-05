e9162dc208c767bfa518dd938b4014a4a06c255b
cjnour <charbel.nour9@gmail.com>
2023-05-05 09:12:38 -0400

    Went back in time.

--- mouseListener.js
+++ mouseListener.js
@@ -3,17 +3,17 @@ let y = 0;
 
 window.addEventListener("mouseup", function (event) {
   x = event.layerX;
-  y = event.layerY - 500;
+  y = event.layerY;
   console.log("js", "\nX: ", x, "\nY: ", y);
   const modal = document.getElementById("modal");
   console.log(modal);
-  modal.style.setProperty(`--x`, `${x}px`);
-  modal.style.setProperty(`--y`, `${y}px`);
+  // modal.style.setProperty(`--x`, `${x}px`);
+  // modal.style.setProperty(`--y`, `${y}px`);
 });
 
-chrome.contextMenus.onClicked.addListener(function (clickData) {
-  console.log("Clicked context menu...mouselistener");
-  const modal = document.getElementById("modal");
-  modal.style.setProperty(`--width`, "400px");
-  modal.style.setProperty(`--height`, "500px");
-});
+// chrome.contextMenus.onClicked.addListener(function (clickData) {
+//   console.log("Clicked context menu...mouselistener");
+//   const modal = document.getElementById("modal");
+//   modal.style.setProperty(`--width`, "400px");
+//   modal.style.setProperty(`--height`, "500px");
+// });
